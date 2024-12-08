6c3d0b02086dc7d131893b2b367fc40de4309d67
Joey Grady <reasonwriter703@gmail.com>
2024-12-07 17:58:32 -0800

    refactored - replaced eval()

--- Day7b.py
+++ Day7b.py
@@ -12,7 +12,14 @@ def make_the_math_math(nums, opers, running_total=0):
     if running_total > total: return 0
     if running_total == 0: running_total = nums.pop(0)
     if len(opers):
-        return make_the_math_math(nums, opers, eval(str(running_total)+opers.pop(0)+str(nums.pop(0))))
+        match opers.pop(0):
+            case '':
+                running_total = int(str(running_total) + str(nums.pop(0)))
+            case '*':
+                running_total = running_total * nums.pop(0)
+            case '+':
+                running_total = running_total + nums.pop(0)
+        return make_the_math_math(nums, opers, running_total)
     else:
         return running_total
 
@@ -21,14 +28,14 @@ with open('Day7-input.txt', 'r') as file:
     for line in file:
         total, numbers = line.strip().split(':')
         total = int(total)
-        numbers = [n for n in numbers.strip().split()]
+        numbers = [int(n) for n in numbers.strip().split()]
 
         # run thru every permutation of operators. 
         # Start with ops resulting in higher running totals to break out of incorrect loops faster
         for oper_iter in (itertools.product(['','*','+'], repeat=len(numbers)-1)):
             if total == make_the_math_math(numbers.copy(), list(oper_iter)):
-                equation = [x for pair in zip(numbers, oper_iter) for x in pair]
-                equation = ''.join(equation)+numbers[-1]
+                equation = [x for pair in zip([str(n) for n in numbers], oper_iter) for x in pair]
+                equation = ''.join(equation)+str(numbers[-1])
                 print(total, equation)
                 result += total
                 break
